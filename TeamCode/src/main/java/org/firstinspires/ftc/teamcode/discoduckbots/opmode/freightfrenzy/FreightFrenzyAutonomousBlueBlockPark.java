1730eec23a5b515bbf09d63233001e1aa9d4353d
dustingood <AbigailNevaehMason>
2021-12-11 11:55:45 -0600

    12/10

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/discoduckbots/opmode/freightfrenzy/FreightFrenzyAutonomousBlueBlockPark.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/discoduckbots/opmode/freightfrenzy/FreightFrenzyAutonomousBlueBlockPark.java
@@ -70,7 +70,7 @@ public class FreightFrenzyAutonomousBlueBlockPark extends LinearOpMode {
     private static final double STRAFE_SPEED = 0.35;
     private static final double ROTATION_SPEED = 0.4;
     private static final int WOBBLE_GRABBER_REVOLUTIONS = 6250;
-    private static final int LEVEL_1 = 3210;
+    private static final int LEVEL_1 = 3230;
     private static final int LEVEL_2 = 4485;
     private static final int LEVEL_3 = 5175;
   /* Note: This sample uses the all-objects Tensor Flow model (FreightFrenzy_BCDM.tflite), which contains
